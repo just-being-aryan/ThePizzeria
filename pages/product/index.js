354ee388b3eabc5873df1de5a973f201a916e1e7
just-being-aryan <aryansachdevaa@gmail.com>
2025-06-29 23:45:13 +0530

    Temp fix of index.js in pages/product

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,32 +1,42 @@
-import dbConnect from "../../util/mongo.js";
-import Product from "../../models/Product";
+// import dbConnect from "../../util/mongo.js";
+// import Product from "../../models/Product";
 
 
-export default async function handler(req, res) {
-  const { method, cookies } = req;
+// export default async function handler(req, res) {
+//   const { method, cookies } = req;
 
-  const token = cookies.token
+//   const token = cookies.token
 
-  dbConnect();
+//   dbConnect();
 
-  if (method === "GET") {
-    try {
-      const products = await Product.find();
-      res.status(200).json(products);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  }
+//   if (method === "GET") {
+//     try {
+//       const products = await Product.find();
+//       res.status(200).json(products);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   }
 
-  if (method === "POST") {
-    if(!token || token !== process.env.token){
-      return res.status(401).json("Not authenticated!")
-    }
-    try {
-      const product = await Product.create(req.body);
-      res.status(201).json(product);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  }
-}
+//   if (method === "POST") {
+//     if(!token || token !== process.env.TOKEN){
+//       return res.status(401).json("Not authenticated!")
+//     }
+//     try {
+//       const product = await Product.create(req.body);
+//       res.status(201).json(product);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   }
+// }
+
+
+
+import React from "react";
+
+const ProductPage = () => {
+  return <div>This is the Product page UI</div>;
+};
+
+export default ProductPage;
